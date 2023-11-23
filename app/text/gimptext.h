ruizlo/gimpmain

text: Add additional outline options
This creates a new "outline direction" property, that determines whether
the outline grows outward, inward, or centered from the text.

app/text/gimptext.h
@@ -39,42 +39,43 @@ struct _GimpText
 {
   GimpObject             parent_instance;
 
-  gchar                 *text;
-  gchar                 *markup;
-  gchar                 *font;
-  GimpUnit               unit;
-  gdouble                font_size;
-  gboolean               antialias;
-  GimpTextHintStyle      hint_style;
-  gboolean               kerning;
-  gchar                 *language;
-  GimpTextDirection      base_dir;
-  GimpRGB                color;
-  GimpFillStyle          outline_style;
-  GimpPattern           *outline_pattern;
-  GimpRGB                outline_foreground;
-  gdouble                outline_width;
-  GimpCapStyle           outline_cap_style;
-  GimpJoinStyle          outline_join_style;
-  gdouble                outline_miter_limit;
-  gboolean               outline_antialias;
-  gdouble                outline_dash_offset;
-  GArray                *outline_dash_info;
-  GimpTextOutline        outline;
-  GimpTextJustification  justify;
-  gdouble                indent;
-  gdouble                line_spacing;
-  gdouble                letter_spacing;
-  GimpTextBoxMode        box_mode;
-  gdouble                box_width;
-  gdouble                box_height;
-  GimpUnit               box_unit;
-  GimpMatrix2            transformation;
-  gdouble                offset_x;
-  gdouble                offset_y;
+  gchar                    *text;
+  gchar                    *markup;
+  gchar                    *font;
+  GimpUnit                  unit;
+  gdouble                   font_size;
+  gboolean                  antialias;
+  GimpTextHintStyle         hint_style;
+  gboolean                  kerning;
+  gchar                    *language;
+  GimpTextDirection         base_dir;
+  GimpRGB                   color;
+  GimpFillStyle             outline_style;
+  GimpPattern              *outline_pattern;
+  GimpRGB                   outline_foreground;
+  gdouble                   outline_width;
+  GimpTextOutlineDirection  outline_direction;
+  GimpCapStyle              outline_cap_style;
+  GimpJoinStyle             outline_join_style;
+  gdouble                   outline_miter_limit;
+  gboolean                  outline_antialias;
+  gdouble                   outline_dash_offset;
+  GArray                   *outline_dash_info;
+  GimpTextOutline           outline;
+  GimpTextJustification     justify;
+  gdouble                   indent;
+  gdouble                   line_spacing;
+  gdouble                   letter_spacing;
+  GimpTextBoxMode           box_mode;
+  gdouble                   box_width;
+  gdouble                   box_height;
+  GimpUnit                  box_unit;
+  GimpMatrix2               transformation;
+  gdouble                   offset_x;
+  gdouble                   offset_y;
 
-  gdouble                border;
-  Gimp                  *gimp;
+  gdouble                   border;
+  Gimp                     *gimp;
 };
 
 struct _GimpTextClass

app/tools/gimptextoptions.h
@@ -37,38 +37,39 @@ struct _GimpTextOptions
 {
   GimpToolOptions        tool_options;
 
-  GimpUnit               unit;
-  gdouble                font_size;
-  gboolean               antialias;
-  GimpTextHintStyle      hint_style;
-  gchar                 *language;
-  GimpTextDirection      base_dir;
-  GimpTextJustification  justify;
-  gdouble                indent;
-  gdouble                line_spacing;
-  gdouble                letter_spacing;
-  GimpTextBoxMode        box_mode;
+  GimpUnit                  unit;
+  gdouble                   font_size;
+  gboolean                  antialias;
+  GimpTextHintStyle         hint_style;
+  gchar                    *language;
+  GimpTextDirection         base_dir;
+  GimpTextJustification     justify;
+  gdouble                   indent;
+  gdouble                   line_spacing;
+  gdouble                   letter_spacing;
+  GimpTextBoxMode           box_mode;
 
-  GimpTextOutline        outline;
-  GimpFillStyle          outline_style;
-  GimpRGB                outline_foreground;
-  GimpPattern           *outline_pattern;
-  gdouble                outline_width;
-  GimpUnit               outline_unit;
-  GimpCapStyle           outline_cap_style;
-  GimpJoinStyle          outline_join_style;
-  gdouble                outline_miter_limit;
-  gboolean               outline_antialias;
-  gdouble                outline_dash_offset;
-  GArray                *outline_dash_info;
+  GimpTextOutline           outline;
+  GimpFillStyle             outline_style;
+  GimpRGB                   outline_foreground;
+  GimpPattern              *outline_pattern;
+  gdouble                   outline_width;
+  GimpTextOutlineDirection  outline_direction;
+  GimpUnit                  outline_unit;
+  GimpCapStyle              outline_cap_style;
+  GimpJoinStyle             outline_join_style;
+  gdouble                   outline_miter_limit;
+  gboolean                  outline_antialias;
+  gdouble                   outline_dash_offset;
+  GArray                   *outline_dash_info;
 
-  GimpViewType           font_view_type;
-  GimpViewSize           font_view_size;
+  GimpViewType              font_view_type;
+  GimpViewSize              font_view_size;
 
-  gboolean               use_editor;
+  gboolean                  use_editor;
 
   /*  options gui  */
-  GtkWidget             *size_entry;
+  GtkWidget                *size_entry;
 };
 
 

sysfce2/gimp

app/text/gimptext.h
@@ -39,42 +39,43 @@ struct _GimpText
 {
   GimpObject             parent_instance;
 
-  gchar                 *text;
-  gchar                 *markup;
-  GimpFont              *font;
-  GimpUnit               unit;
-  gdouble                font_size;
-  gboolean               antialias;
-  GimpTextHintStyle      hint_style;
-  gboolean               kerning;
-  gchar                 *language;
-  GimpTextDirection      base_dir;
-  GimpRGB                color;
-  GimpCustomStyle        outline_style;
-  GimpPattern           *outline_pattern;
-  GimpRGB                outline_foreground;
-  gdouble                outline_width;
-  GimpCapStyle           outline_cap_style;
-  GimpJoinStyle          outline_join_style;
-  gdouble                outline_miter_limit;
-  gboolean               outline_antialias;
-  gdouble                outline_dash_offset;
-  GArray                *outline_dash_info;
-  GimpTextOutline        outline;
-  GimpTextJustification  justify;
-  gdouble                indent;
-  gdouble                line_spacing;
-  gdouble                letter_spacing;
-  GimpTextBoxMode        box_mode;
-  gdouble                box_width;
-  gdouble                box_height;
-  GimpUnit               box_unit;
-  GimpMatrix2            transformation;
-  gdouble                offset_x;
-  gdouble                offset_y;
-
-  gdouble                border;
-  Gimp                  *gimp;
+  gchar                    *text;
+  gchar                    *markup;
+  GimpFont                 *font;
+  GimpUnit                  unit;
+  gdouble                   font_size;
+  gboolean                  antialias;
+  GimpTextHintStyle         hint_style;
+  gboolean                  kerning;
+  gchar                    *language;
+  GimpTextDirection         base_dir;
+  GimpRGB                   color;
+  GimpCustomStyle           outline_style;
+  GimpPattern              *outline_pattern;
+  GimpRGB                   outline_foreground;
+  gdouble                   outline_width;
+  GimpTextOutlineDirection  outline_direction;
+  GimpCapStyle              outline_cap_style;
+  GimpJoinStyle             outline_join_style;
+  gdouble                   outline_miter_limit;
+  gboolean                  outline_antialias;
+  gdouble                   outline_dash_offset;
+  GArray                   *outline_dash_info;
+  GimpTextOutline           outline;
+  GimpTextJustification     justify;
+  gdouble                   indent;
+  gdouble                   line_spacing;
+  gdouble                   letter_spacing;
+  GimpTextBoxMode           box_mode;
+  gdouble                   box_width;
+  gdouble                   box_height;
+  GimpUnit                  box_unit;
+  GimpMatrix2               transformation;
+  gdouble                   offset_x;
+  gdouble                   offset_y;
+                           
+  gdouble                   border;
+  Gimp                     *gimp;
 };
 
 struct _GimpTextClass

app/tools/gimptextoptions.h
@@ -37,39 +37,40 @@ struct _GimpTextOptions
 {
   GimpToolOptions        tool_options;
 
-  GimpUnit               unit;
-  gdouble                font_size;
-  gboolean               antialias;
-  GimpTextHintStyle      hint_style;
-  gchar                 *language;
-  GimpTextDirection      base_dir;
-  GimpTextJustification  justify;
-  gdouble                indent;
-  gdouble                line_spacing;
-  gdouble                letter_spacing;
-  GimpTextBoxMode        box_mode;
+  GimpUnit                  unit;
+  gdouble                   font_size;
+  gboolean                  antialias;
+  GimpTextHintStyle         hint_style;
+  gchar                    *language;
+  GimpTextDirection         base_dir;
+  GimpTextJustification     justify;
+  gdouble                   indent;
+  gdouble                   line_spacing;
+  gdouble                   letter_spacing;
+  GimpTextBoxMode           box_mode;
+                           
+  GimpTextOutline           outline;
+  GimpCustomStyle           outline_style;
+  GimpRGB                   outline_foreground;
+  GimpPattern              *outline_pattern;
+  gdouble                   outline_width;
+  GimpTextOutlineDirection  outline_direction;
+  GimpUnit                  outline_unit;
+  GimpCapStyle              outline_cap_style;
+  GimpJoinStyle             outline_join_style;
+  gdouble                   outline_miter_limit;
+  gboolean                  outline_antialias;
+  gdouble                   outline_dash_offset;
+  GArray                   *outline_dash_info;
 
-  GimpTextOutline        outline;
-  GimpCustomStyle        outline_style;
-  GimpRGB                outline_foreground;
-  GimpPattern           *outline_pattern;
-  gdouble                outline_width;
-  GimpUnit               outline_unit;
-  GimpCapStyle           outline_cap_style;
-  GimpJoinStyle          outline_join_style;
-  gdouble                outline_miter_limit;
-  gboolean               outline_antialias;
-  gdouble                outline_dash_offset;
-  GArray                *outline_dash_info;
-
-  GimpViewType           font_view_type;
-  GimpViewSize           font_view_size;
-
-  gboolean               use_editor;
-  gboolean               show_on_canvas;
+  GimpViewType              font_view_type;
+  GimpViewSize              font_view_size;
+                            
+  gboolean                  use_editor;
+  gboolean                  show_on_canvas;
 
   /*  options gui  */
-  GtkWidget             *size_entry;
+  GtkWidget                *size_entry;
 };
 
 